987318ff73da60200f5e5e0a20ca23c757de6db8
Jef King <jef@agilebusinesscloud.com>
2015-10-31 22:50:04 -0700

    remove code

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,29 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace web.Controllers
+﻿namespace web.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
