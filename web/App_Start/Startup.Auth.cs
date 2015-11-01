ede6e98db5aeaf2ec3eb5be9f3232e7257969e99
Jef King <jef@agilebusinesscloud.com>
2015-10-31 22:49:20 -0700

    the carver

--- web/App_Start/Startup.Auth.cs
+++ web/App_Start/Startup.Auth.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using web.Models;
 
 namespace web
 {
