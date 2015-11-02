0e897b9a2dbd4fdeb83ca00265d17a12ee060dc5
Jef King <jef@agilebusinesscloud.com>
2015-11-01 21:32:00 -0800

    getting there

---
+++ web/Controllers/NutritionController.cs
@@ -0,0 +1,29 @@
+﻿namespace web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Web.Http;
+    using King.Azure.Data;
+    using Models;
+
+    public class NutritionController : ApiController
+    {
+        #region Members
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["datastore"].ConnectionString;
+        private readonly ITableStorage storage = null;
+        #endregion
+
+        public NutritionController()
+            : this(new TableStorage("foods", connectionString))
+        { }
+        public NutritionController(ITableStorage storage)
+        { }
+
+
+        [HttpGet]
+        public IEnumerable<Food> Get()
+        {
+            return null;
+        }
+    }
+}
