4cd1a46ca974be77ba995047a0b7895986fd733f
Jef King <jef@agilebusinesscloud.com>
2015-11-03 22:17:23 -0800

    formatting

--- web/Scripts/foodTable.jsx
+++ web/Scripts/foodTable.jsx
@@ -1,34 +1,34 @@
-﻿var CommentBox = React.createClass({
+﻿var DataTable = React.createClass({
     render: function() {
         return (
           <table class="table table-hover">
-                <tr>
-                    <td>Name</td>
-                    <td>Fat</td>
-                    <td>Carbs</td>
-                    <td>Protien</td>
-                    <td>Fat %</td>
-                    <td>Carb %</td>
-                    <td>Protien %</td>
-                    <td>Cal/100gr</td>
-                    <td>Sparkline</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+            <tr>
+                <td>Name</td>
+                <td>Fat</td>
+                <td>Carbs</td>
+                <td>Protien</td>
+                <td>Fat %</td>
+                <td>Carb %</td>
+                <td>Protien %</td>
+                <td>Cal/100gr</td>
+                <td>Sparkline</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
       );
     }
 });
 React.render(
-  <CommentBox />,
+  <DataTable />,
   document.getElementById('data')
 );
