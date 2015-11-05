1d12d9ea7117106b96914680e65b7782164b12f0
Jef King <jef@agilebusinesscloud.com>
2015-11-04 20:32:10 -0800

    more betters

---
+++ web/Scripts/foodDisplay.jsx
@@ -0,0 +1,41 @@
+﻿var DataTable = React.createClass({
+    render: function () {
+        return (
+            <table class="table table-hover">
+                <tr>
+                    <td>Name</td>
+                    <td>Fat</td>
+                    <td>Carbs</td>
+                    <td>Protien</td>
+                    <td>Fat %</td>
+                    <td>Carb %</td>
+                    <td>Protien %</td>
+                    <td>Cal/100gr</td>
+                    <td>Sparkline</td>
+                </tr>
+                <DataRows data={this.props.data} />
+                </table>
+        );
+    }
+});
+var DataRows = React.createClass({
+    render: function (food) {
+        return (
+            <tr>
+                <td>b</td>
+                <td>a</td>
+                <td>r</td>
+                <td>e</td>
+                <td>d</td>
+                <td>b</td>
+                <td>a</td>
+                <td>z</td>
+                <td>x</td>
+            </tr>
+        );
+    }
+});
+React.render(
+    <DataTable url="/api/nutrition" />,
+    document.getElementById('data')
+);
