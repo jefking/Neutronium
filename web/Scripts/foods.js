a4f3e67e87dacd9a62da185f0dbeb3210845ea7e
Jef King <jef@agilebusinesscloud.com>
2015-11-05 21:34:32 -0800

    Swtich up

--- web/Scripts/foods.js
+++ web/Scripts/foods.js
@@ -3,7 +3,7 @@
 var nutritionApp = angular.module('nutritionApp', []);
 
 nutritionApp.controller('nutrition', ['$scope', '$http', function ($scope, $http) {
-    $http.get('/Scripts/foodData.json').success(function (data) {
+    $http.get('/Scripts/foodData.js').success(function (data) {
         $scope.foods = data;
     });
 }]);
