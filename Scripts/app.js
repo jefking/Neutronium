b06d259ccb49e4561131ea69bed37a1b4e3c58f7
Jef King <jefkin@microsoft.com>
2015-11-08 11:57:03 -0700

    Sorting in place

---
+++ Scripts/app.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+var nutritionApp = angular.module('nutritionApp', []);
+var Foods;
+
+nutritionApp.controller('nutrition', ['$scope', '$http', function ($scope, $http) {
+    $http.get('Scripts/data.js').success(function (data) {
+        $scope.foods = data;
+        Foods = data;
+    });
+}]);
+
+nutritionApp.controller('picked', ['$scope', function ($scope) {
+    //$scope.counter = 0;
+    $scope.change = function() {
+        //$scope.counter++;
+    };
+}]);
+
+nutritionApp.controller('sort', ['$scope', function($scope) {
+    $scope.foods = Foods;
+    $scope.predicate = 'Name';
+    $scope.reverse = true;
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    }
+}]);
