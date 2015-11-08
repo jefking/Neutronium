1a0a70bf300e6a850d86981d59b9f2bbe8fd3c14
Jef King <jefkin@microsoft.com>
2015-11-08 12:57:35 -0700

    Adding item to array

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,8 @@
 ﻿'use strict';
 
 var nutritionApp = angular.module('nutritionApp', []);
+var selected = [];
+
 
 nutritionApp.controller('nutrition', ['$scope', '$http', function ($scope, $http) {
     $http.get('Scripts/data.js').success(function (data) {
@@ -12,6 +14,8 @@ nutritionApp.controller('picked', ['$scope', function ($scope) {
     //$scope.counter = 0;
     $scope.change = function() {
         //$scope.counter++;
+        selected.push($scope.food);
+        console.log(selected);
     };
 }]);
 
