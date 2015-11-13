7354a6e392fc9c474805afb3f15faea58209eb83
Jef King <jefkin@microsoft.com>
2015-11-12 20:20:23 -0800

    First Phase done!

--- Scripts/app.js
+++ Scripts/app.js
@@ -61,3 +61,17 @@ nutritionApp.filter('sumByKey', function() {
         return sum;
     };
 });
+nutritionApp.filter('sumByFunc', function() {
+    return function(data, key) {
+        if (typeof(data) === 'undefined' || typeof(key) === 'undefined') {
+            return 0;
+        }
+
+        var sum = 0;
+        for (var i = data.length - 1; i >= 0; i--) {
+            sum += parseInt(data[i][key]());
+        }
+
+        return sum;
+    };
+});
